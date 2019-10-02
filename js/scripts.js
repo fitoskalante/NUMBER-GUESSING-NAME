339c9699ff7aa3aee5aa5a6e7a3d300c1595b12b
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-10-03 02:28:51 +0700

    styling in process

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+let time = 30;
+let myTime;
+
+function setTime() {
+    time -= 1;
+    document.getElementById("time").innerHTML = time;
+
+    if (time == 0) {
+        stopTimer();
+        resultArea.innerHTML = "Game over!";
+    }
+}
+
+function timeCounting() {
+    myTime = setInterval(setTime, 1000)
+}
+function stopTimer() {
+    clearInterval(myTime);
+    time = 30;
+    document.getElementById("time").innerHTML = time;
+}
+
 let random = Math.floor((Math.random() * 100) + 1);
 console.log(';random', random)
 
@@ -13,13 +35,13 @@ function guess() {
         resultArea.innerHTML = "you have already said that number!";
         return;
     }
-    if (history.length === 9) {
+    if (history.length === 4) {
         text = "Game over!";
         history.push(guessNumber);
         document.getElementById("guessButton").disabled = true;
     } else if (guessNumber > random) {
         history.push(guessNumber);
-        text = "this number is too big!";
+        text= "this number is too big!";
     } else if (guessNumber < random) {
         history.push(guessNumber);
         text = "this number is too low!!";
@@ -33,9 +55,8 @@ function guess() {
 
 function reset() {
     history = [];
-    historyCheck.innerHTML = "";
+    historyCheck.innerHTML = "your attempts";
     document.getElementById("myGuess").value = "";
     document.getElementById("guessButton").disabled = false;
-    resultArea.innerHTML = "";
-} 
-
+    resultArea.innerHTML = "Let's try again!";
+}
